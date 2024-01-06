79db2b9831c9b100411c2b08633b601acb1359a9
Parth Puranik <thosparth@gmail.com>
2024-01-06 20:18:27 +0530

    g

--- jsfiles/auth.js
+++ jsfiles/auth.js
@@ -1,6 +1,6 @@
 const authTokenSpotify = localStorage.getItem("authToken");
-const client_id = "40e0eacdf8d744fe8bb0946f7091daf8";
-const redirect_uri = "http://127.0.0.1:5500/index.html";
+// const client_id = "40e0eacdf8d744fe8bb0946f7091daf8";
+// const redirect_uri = "http://127.0.0.1:5500/index.html";
 const scope = [
   "user-read-email",
   "app-remote-control",
